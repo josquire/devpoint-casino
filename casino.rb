34b319c471bbf40a4d6ea94f1fd488f7ee3b77d1
Charles Brown <adsr13@gmail.com>
2016-08-09 11:56:20 -0600

    player exits slots and returns to menu

--- casino.rb
+++ casino.rb
@@ -69,23 +69,23 @@ class Casino
     if player  == 'a'
       playing_slots
     elsif player == 'b'
-      goodbye
+     Casino.menu
     end
   end
-  
+
   def playing_slots
-    
+
     slot_options = ["grapes", "strawberries", "lemons", "avocado"]
-    col_1 = slot_options.sample 
-    col_2 = slot_options.sample 
-    col_3 = slot_options.sample 
+    col_1 = slot_options.sample
+    col_2 = slot_options.sample
+    col_3 = slot_options.sample
     puts "#{col_1} #{col_2} #{col_3} "
     if col_1 == col_2 && col_1 == col_3
       puts 'you win $10,000'.colorize(:blue)
       # @bank_roll += 10000
       @player.update_balance('+', 10000)
       repeat_slots
-    else 
+    else
       puts 'you lose $50'.colorize(:red)
       @player.update_balance('-', 50)
       repeat_slots
@@ -105,9 +105,9 @@ class Casino
 #         goodbye
 #       end
 #   end
- 
+
 #   def black_jack
-#   end 
+#   end
 #   repeat_black_jack
 # end
 
